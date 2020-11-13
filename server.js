606eda17a19c98a7acc84b7e0005000ac3b5eccd
Ania <kubowania@gmail.com>
2020-11-13 11:39:43 +0000

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
